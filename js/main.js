f9bfe3b2dcb3b0414a72a24685a351fed4b11a61
StepikovaOlya <zagornianu@gmail.com>
2020-03-03 11:03:23 +0200

    Section_about

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//$(function () {
+//    $('#nav ul li').hover(
+//        function () {
+//            $('ul', this).stop().slideDown(400);
+//        },
+//        function () {
+//            $('ul', this).stop().slideUp(400);
+//        }
+//    );
+//});
+
+
+
+$(function () {
+    $('.menu').menu();
+});
